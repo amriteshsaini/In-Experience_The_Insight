1e49b05458db07f044bb4f8c7d038a92f729f701
purvii05 <39964131+purvii05@users.noreply.github.com>
2020-08-17 04:16:53 +0530

    Menu options added

---
+++ Assets/Menu/Scenechange.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenechange : MonoBehaviour
+{
+    public void ChangeToScene (string sceneToChangeto)
+    {
+        Application.LoadLevel(sceneToChangeto);
+    }
+}
